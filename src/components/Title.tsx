674d1e21466179fd4728946841acbdcd00191dbd
SuimKim <rrrr6563@naver.com>
2025-03-12 14:51:48 +0900

    feat: 정렬 위치 옵션 추가

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,23 +1,30 @@
 import { ReactNode } from "react";
-
 /**
  * 제목 컴포넌트입니다. font-bold 속성이 기본 설정되어있습니다.
  * @component
  *
- * @prop {string} [size] - 글자 크기 ( "xl" | "2xl" | "3xl" | "4xl" | "5xl" )
- * @prop {string} [margin] - 마진 크기 ( "sm" | "md" | "lg" | "xl" | "2xl" )
+ * @prop {Size} [size] - 글자 크기 ( "xl" | "2xl" | "3xl" | "4xl" | "5xl" )
+ * @prop {Margin} [margin] - 마진 크기 ( "sm" | "md" | "lg" | "xl" | "2xl" )
+ * @prop {Align} [align] - 정렬 위치 ( "left" | "center" | "right" )
  */
 type Size = "xl" | "2xl" | "3xl" | "4xl" | "5xl";
-type Margin = "sm" | "md" | "lg" | "xl" | "2xl";
+type Margin = "none" | "sm" | "md" | "lg" | "xl" | "2xl";
+type Align = "left" | "center" | "right";
 
 type titleProps = {
   size: Size;
   margin: Margin;
+  align: Align;
   children: ReactNode;
 };
 
-const Title = ({ size, margin, children }: titleProps) => {
-  const baseStyles = "font-bold";
+const Title = ({
+  size = "xl",
+  margin = "none",
+  align = "left",
+  children,
+}: titleProps) => {
+  const baseStyles = "font-bold justify-items-center";
 
   const sizeStyles: Record<Size, string> = {
     xl: "text-xl",
@@ -28,6 +35,7 @@ const Title = ({ size, margin, children }: titleProps) => {
   };
 
   const marginStyles: Record<Margin, string> = {
+    none: "",
     sm: "m-2",
     md: "m-4",
     lg: "m-6",
@@ -35,7 +43,9 @@ const Title = ({ size, margin, children }: titleProps) => {
     "2xl": "m-10",
   };
 
-  const styles = `${baseStyles} ${sizeStyles[size]} ${marginStyles[margin]}`;
+  const alignStyles: string = `text-${align}`;
+
+  const styles = `${baseStyles} ${sizeStyles[size]} ${marginStyles[margin]} ${alignStyles}`;
 
   return <h1 className={styles}>{children}</h1>;
 };
