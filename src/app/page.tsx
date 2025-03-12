9743a244498da2593e83bb8c03186506e0bf7fef
SuimKim <rrrr6563@naver.com>
2025-03-12 10:57:58 +0900

    feat:이미지 도메인 및 path 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+import Image from "next/image";
+
 export default function Home() {
+  const championsImgPath: string =
+    "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news_live/64008dd57fddfa0a3baad95b72e3bdd13068a97f-1920x1080.jpg";
+  const lotationsImgPath: string =
+    "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news_live/510633f8ba622a63f02a88a96b2a28422180d735-1920x1080.jpg";
+  const itemsImgPath: string =
+    "https://cmsassets.rgpub.io/sanity/images/dsfx7636/universe_live/7b11239cf024358011444e7d70bb2badbb6b33e2-6000x3155.jpg";
   return (
     <main className="mt-24">
       <section className="text-center">
