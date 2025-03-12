780c4991ca4a50db4e2773c7b88daa8be28388ee
SuimKim <rrrr6563@naver.com>
2025-03-12 10:51:10 +0900

    feat: 타이틀 섹션 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>홈페이지</div>;
+  return (
+    <main className="mt-24">
+      <section className="text-center">
+        <h1 className="font-bold text-4xl">리그오브레전드 정보 앱</h1>
+        <p className="mt-5">
+          Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
+        </p>
+      </section>
+    </main>
+  );
 }
