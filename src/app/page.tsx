3c51dd9b1153707485246eeaf25b5bc800713c48
SuimKim <rrrr6563@naver.com>
2025-03-12 17:06:07 +0900

    refactor: 카드 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,39 @@
-import Text from "@/components/Text";
-import Title from "@/components/Title";
+import Text from "@/components/common/Text";
+import Title from "@/components/common/Title";
 import Image from "next/image";
 import Link from "next/link";
 
+type CardData = {
+  linkHref: string;
+  imgSrc: string;
+  imgAlt: string;
+  textLabel: string;
+};
+
 export default function Home() {
-  const championsImgPath: string =
-    "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news_live/64008dd57fddfa0a3baad95b72e3bdd13068a97f-1920x1080.jpg";
-  const rotationsImgPath: string =
-    "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news_live/510633f8ba622a63f02a88a96b2a28422180d735-1920x1080.jpg";
-  const itemsImgPath: string =
-    "https://cmsassets.rgpub.io/sanity/images/dsfx7636/universe_live/7b11239cf024358011444e7d70bb2badbb6b33e2-6000x3155.jpg";
+  const mainCardData: CardData[] = [
+    {
+      linkHref: "/champion",
+      imgSrc:
+        "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news_live/64008dd57fddfa0a3baad95b72e3bdd13068a97f-1920x1080.jpg",
+      imgAlt: "챔피언 목록",
+      textLabel: "챔피언 목록 보기",
+    },
+    {
+      linkHref: "/rotation",
+      imgSrc:
+        "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news_live/510633f8ba622a63f02a88a96b2a28422180d735-1920x1080.jpg",
+      imgAlt: "금주 로테이션",
+      textLabel: "금주 로테이션 확인",
+    },
+    {
+      linkHref: "/champion",
+      imgSrc:
+        "https://cmsassets.rgpub.io/sanity/images/dsfx7636/universe_live/7b11239cf024358011444e7d70bb2badbb6b33e2-6000x3155.jpg",
+      imgAlt: "아이템 목록",
+      textLabel: "아이템 목록 보기",
+    },
+  ];
 
   return (
     <main className="mt-24">
@@ -22,40 +46,35 @@ export default function Home() {
         </Text>
       </section>
       <section>
-        <Link href={"/champion"} className="flex flex-col items-center">
-          <Image
-            src={championsImgPath}
-            width={500}
-            height={500}
-            alt="챔피언 목록"
+        {mainCardData.map((data, index) => (
+          <MainCard
+            key={index}
+            linkHref={data.linkHref}
+            imgSrc={data.imgSrc}
+            imgAlt={data.imgAlt}
+            textLabel={data.textLabel}
           />
-          <Text size="md" align="center">
-            챔피언 목록 보기
-          </Text>
-        </Link>
-        <Link href={"/rotation"} className="flex flex-col items-center">
-          <Image
-            src={rotationsImgPath}
-            width={500}
-            height={500}
-            alt="금주 로테이션"
-          />
-          <Text size="md" align="center">
-            금주 로테이션 확인
-          </Text>
-        </Link>
-        <Link href={"/items"} className="flex flex-col items-center">
-          <Image
-            src={itemsImgPath}
-            width={500}
-            height={500}
-            alt="아이템 목록"
-          />
-          <Text size="md" align="center">
-            아이템 목록 보기
-          </Text>
-        </Link>
+        ))}
       </section>
     </main>
   );
 }
+
+type MainCardProps = {
+  linkHref: string;
+  imgSrc: string;
+  imgAlt: string;
+  textLabel: string;
+};
+
+const MainCard = ({ linkHref, imgSrc, imgAlt, textLabel }: MainCardProps) => {
+  return (
+    <Link href={linkHref} className="flex flex-col items-center">
+      <Image src={imgSrc} alt={imgAlt} width={600} height={400} />
+
+      <Text size="md" align="center">
+        {textLabel}
+      </Text>
+    </Link>
+  );
+};
