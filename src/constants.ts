972d3f2fe9896467fb5dc3c98d314751ab1bf621
SuimKim <rrrr6563@naver.com>
2025-03-13 21:20:13 +0900

    refactor: 이미지 패스 상수화

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const ITEM_IMG_PATH: string =
+  "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/item";
+
+export const CHAMPION_LIST_IMG_PATH: string =
+  "https://ddragon.leagueoflegends.com/cdn/img/champion/loading";
+
+export const CHAMPION_DETAIL_IMG_PATH: string =
+  "https://ddragon.leagueoflegends.com/cdn/img/champion/splash";
+
+export const SPELL_IMG_PATH: string =
+  "https://ddragon.leagueoflegends.com/cdn/14.5.1/img/spell";
