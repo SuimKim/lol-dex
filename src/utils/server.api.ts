1f64838ffedab6e5f0998914661799d5e1144c0d
SuimKim <rrrr6563@naver.com>
2025-03-14 17:46:03 +0900

    refactor: 디테일 페이지 fetch 로직 분리

--- src/utils/server.api.ts
+++ src/utils/server.api.ts
@@ -1,4 +1,4 @@
-import { Champion } from "@/types/shared/api/champion.types";
+import { Champion, ChampionDetail } from "@/types/shared/riot.api.types";
 
 const getVersion: () => Promise<string[]> = async () => {
   const res: Response = await fetch(
@@ -21,5 +21,20 @@ export const getChampionList: () => Promise<{
     }
   );
   const { data }: { data: { [key: string]: Champion } } = await res.json();
+  return data;
+};
+
+export const getChampionDetail: (id: string) => Promise<{
+  [key: string]: ChampionDetail;
+}> = async (id) => {
+  const version = await getVersion();
+  const res: Response = await fetch(
+    `https://ddragon.leagueoflegends.com/cdn/${version[0]}/data/ko_KR/champion/${id}.json`,
+    {
+      cache: "no-store",
+    }
+  );
+  const { data }: { data: { [key: string]: ChampionDetail } } =
+    await res.json();
   return data;
 };
