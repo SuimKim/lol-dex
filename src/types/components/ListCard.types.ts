273123f36d49e5d8ecd5f36a34a4eb3b60f9d88b
SuimKim <rrrr6563@naver.com>
2025-03-14 10:20:15 +0900

    refactor: 컴포넌트 공통 사용을 위한 타입 수정 - 유니언 타입으로 Champion과 Item 모두 허용하도록 수정 - ItemCard.types.ts는 중복 코드임으로 삭제

--- src/types/components/ListCard.types.ts
+++ src/types/components/ListCard.types.ts
@@ -1,5 +1,7 @@
 import { Champion } from "../shared/api/champion.types";
+import { Item } from "../shared/api/item.types";
 
-export type ListCardProps = {
-  item: Champion;
+export type ItemCardProps = {
+  item: Champion | Item;
+  imgPath: string;
 };
