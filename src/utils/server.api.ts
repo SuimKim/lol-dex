b47dd13a598be76e458127931cbd3a15ec598bc5
SuimKim <rrrr6563@naver.com>
2025-03-14 19:27:19 +0900

    refactor: Rotation 페이지 fetch 로직 분리

--- src/utils/server.api.ts
+++ src/utils/server.api.ts
@@ -1,4 +1,8 @@
-import { Champion, ChampionDetail } from "@/types/shared/riot.api.types";
+import {
+  Champion,
+  ChampionDetail,
+  ChampionRotation,
+} from "@/types/shared/riot.api.types";
 
 const getVersion: () => Promise<string[]> = async () => {
   const res: Response = await fetch(
@@ -36,5 +40,21 @@ export const getChampionDetail: (id: string) => Promise<{
   );
   const { data }: { data: { [key: string]: ChampionDetail } } =
     await res.json();
+  return data;
+};
+
+export const getRotation: () => Promise<ChampionRotation> = async () => {
+  const apiKey = process.env.RIOT_API_KEY;
+  if (!apiKey) throw new Error("RIOT_API_KEY가 설정되지 않았습니다");
+
+  const res: Response = await fetch(
+    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+    {
+      headers: {
+        "X-Riot-Token": apiKey,
+      },
+    }
+  );
+  const data = await res.json();
   return data;
 };
