11cb70be2c46070456eab4d63f53b91cacc5f6d1
SuimKim <rrrr6563@naver.com>
2025-03-14 15:43:40 +0900

    refactor: 함수 타입 명시 추가

--- src/utils/server.api.ts
+++ src/utils/server.api.ts
@@ -1,15 +1,16 @@
-import { ChampionData } from "@/types/shared/api/champion.types";
+import { Champion, ChampionData } from "@/types/shared/api/champion.types";
 
-const getVersion = async () => {
+const getVersion: () => Promise<string[]> = async () => {
   const res: Response = await fetch(
     "https://ddragon.leagueoflegends.com/api/versions.json"
   );
-
   const data: string[] = await res.json();
   return data;
 };
 
-export const getChampionList = async () => {
+export const getChampionList: () => Promise<{
+  [key: string]: Champion;
+}> = async () => {
   const version = await getVersion();
   const res: Response = await fetch(
     `https://ddragon.leagueoflegends.com/cdn/${version[0]}/data/ko_KR/champion.json`,
