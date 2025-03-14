cd018036841a5f07c5fe7eada76452513dd4ff36
SuimKim <rrrr6563@naver.com>
2025-03-14 16:00:47 +0900

    refactor: Detail 타입 분리 및 수정 - ChampionDetail과 Data 타입 분리 - ChampionDetail엔 디테일 타입만 들어가도록 변경

---
+++ src/types/shared/api/champion.detail.types.ts
@@ -0,0 +1,82 @@
+export type ChampionDetail = {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+  skins: {
+    id: string;
+    num: number;
+    name: string;
+    chromas: false;
+  }[];
+  lore: string;
+  blurb: string;
+  allytips: string[];
+  enemytips: string[];
+  tags: string[];
+  partype: string;
+  info: { [key: string]: number };
+  stats: { [key: string]: number };
+  spells: Spell[];
+  passive: {
+    name: string;
+    description: string;
+    image: {
+      full: string;
+      sprite: string;
+      group: string;
+      x: number;
+      y: number;
+      w: number;
+      h: number;
+    };
+  };
+  recommended: [];
+};
+
+export type ChampionDetailData = {
+  type: string;
+  format: string;
+  version: string;
+  data: ChampionDetail;
+};
+
+export type Spell = {
+  id: string;
+  name: string;
+  description: string;
+  tooltip: string;
+  leveltip: { [key: string]: string[] };
+  maxrank: number;
+  cooldown: number[];
+  cooldownBurn: string;
+  cost: number[];
+  costBurn: string;
+  datavalues: {};
+  effect: (number[] | null)[];
+  effectBurn: (string | null)[];
+  vars: [];
+  costType: string;
+  maxammo: string;
+  range: number[];
+  rangeBurn: string;
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+  resource: string;
+};
