9424fd58ea7d2aaf4b25591746080a29ab657d0c
SuimKim <rrrr6563@naver.com>
2025-03-15 23:25:49 +0900

    refactor: .env 변수 접두사 명시 - 클라이언트에서 사용하기 위해 .env 환경변수 접두사 명시 - API 로직의 .env 변수명 역시 수정

--- src/utils/server.api.ts
+++ src/utils/server.api.ts
@@ -45,8 +45,8 @@ export const getChampionDetail: (
 };
 
 export const getRotation: () => Promise<ChampionRotation> = async () => {
-  const apiKey = process.env.RIOT_API_KEY;
-  if (!apiKey) throw new Error("RIOT_API_KEY가 설정되지 않았습니다");
+  const apiKey = process.env.NEXT_PUBLIC_RIOT_API_KEY;
+  if (!apiKey) throw new Error("RIOT_API_KEY가 설정되지 않았습니다.");
 
   const res: Response = await fetch(
     "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
