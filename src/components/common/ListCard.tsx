0070a73dd1dccb92807c584f8b2fa90457c9b0c6
SuimKim <rrrr6563@naver.com>
2025-03-16 04:54:58 +0900

    design: List 스타일링

--- src/components/common/ListCard.tsx
+++ src/components/common/ListCard.tsx
@@ -3,13 +3,21 @@ import Title from "./Title";
 import Text from "./Text";
 import { ChildrenProps } from "@/types/shared/common.types";
 import { ListCardProps } from "@/types/components/common.types";
+import Link from "next/link";
 
-const ListCard = ({ item, imgPath }: ListCardProps) => {
+const ListCard = ({ item, imgPath, imgHeight, imgWidth }: ListCardProps) => {
   return (
     <CardContainer>
-      <Image src={imgPath} width={300} height={150} alt="아이템 이미지" />
+      <Image
+        src={imgPath}
+        width={imgHeight}
+        height={imgWidth}
+        alt="아이템 이미지"
+        className="w-full"
+        priority
+      />
       <ContentsContainer>
-        <Title tag="h2" size="xl" align="left" margin="none">
+        <Title tag="h2" size="sm" align="left" margin="none">
           {item.name}
         </Title>
         <Text size="md" align="left">
@@ -23,11 +31,7 @@ const ListCard = ({ item, imgPath }: ListCardProps) => {
 export default ListCard;
 
 const CardContainer = ({ children }: ChildrenProps) => {
-  return (
-    <div className="border-2 border-black flex flex-col items-center">
-      {children}
-    </div>
-  );
+  return <div className="p-2 md:p-4 lg:p-6 flex flex-col">{children}</div>;
 };
 
 const ContentsContainer = ({ children }: ChildrenProps) => {
