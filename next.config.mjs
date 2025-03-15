6b56588356aaf832043297a2db8e29431c512239
SuimKim <rrrr6563@naver.com>
2025-03-16 03:06:36 +0900

    feat: Logo 이미지 추가를 위한 이미지 host 추가

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,17 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "cmsassets.rgpub.io",
-        port: "", // 기본 포트면 빈 문자열
         pathname: "/sanity/images/dsfx7636/**", // 와일드카드로 모든 하위 경로 허용
       },
       {
         protocol: "https",
         hostname: "ddragon.leagueoflegends.com",
-        port: "", // 기본 포트면 빈 문자열
         pathname: "/cdn/**", // 와일드카드로 모든 하위 경로 허용
+      },
+      {
+        protocol: "https",
+        hostname: "brand.riotgames.com",
+        pathname: "/static/**", // 와일드카드로 모든 하위 경로 허용
       },
     ],
   },
