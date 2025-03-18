bac1422254d0c8fe90cb9d46dbc7e9c050cd1f3d
SuimKim <rrrr6563@naver.com>
2025-03-18 11:16:55 +0900

    refactor: 스켈레톤 이미지 타입 분리

--- src/components/common/SkeletonImage.tsx
+++ src/components/common/SkeletonImage.tsx
@@ -2,36 +2,30 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import type { SkeltonImage } from "@/types/components/common.types";
 
 const SkeletonImage = ({
-  src,
-  width,
-  height,
+  imgPath,
+  imgWidth,
+  imgHeight,
   alt,
   priority,
   className,
-}: {
-  src: string;
-  width: number;
-  height: number;
-  alt: string;
-  priority?: boolean;
-  className?: string;
-}) => {
+}: SkeltonImage) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <div
       className="relative w-full "
-      style={{ aspectRatio: `${width}/${height}` }}
+      style={{ aspectRatio: `${imgWidth}/${imgHeight}` }}
     >
       {!isLoaded && (
         <div className="absolute inset-0 bg-gray-300 animate-pulse rounded" />
       )}
       <Image
-        src={src}
-        width={width}
-        height={height}
+        src={imgPath}
+        width={imgWidth}
+        height={imgHeight}
         alt={alt}
         priority={priority}
         onLoadingComplete={() => setIsLoaded(true)}
