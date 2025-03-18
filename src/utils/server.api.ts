98cca36a7ab7e977aaf33de4fc01f16873397908
SuimKim <rrrr6563@naver.com>
2025-03-19 04:08:18 +0900

    refactor: Test용 콘솔 출력 코드 삭제

--- src/utils/server.api.ts
+++ src/utils/server.api.ts
@@ -8,7 +8,6 @@ import type {
 import { GET_ITEMS_URL, GET_VERSION_URL, MY_BUILD_URL } from "@/constants";
 
 const getVersion = async (): Promise<string> => {
-  console.log("버전 API 호출 시간:", new Date().toISOString());
   const res: Response = await fetch(GET_VERSION_URL);
   const data: string[] = await res.json();
   const BASE_URL: string = `https://ddragon.leagueoflegends.com/cdn/${data[0]}/data/ko_KR`;
@@ -18,7 +17,6 @@ const getVersion = async (): Promise<string> => {
 export const getChampionList = async (): Promise<
   fetchReturnValue<Champion>
 > => {
-  console.log("챔피언 리스트 API 호출 시간:", new Date().toISOString());
   const BASE_URL = await getVersion();
   const res: Response = await fetch(`${BASE_URL}/champion.json`, {
     next: {
@@ -32,7 +30,6 @@ export const getChampionList = async (): Promise<
 export const getChampionDetail = async (
   id: string
 ): Promise<fetchReturnValue<ChampionDetail>> => {
-  console.log("디테일 API 호출 시간:", new Date().toISOString());
   const BASE_URL = await getVersion();
   const res: Response = await fetch(`${BASE_URL}/champion/${id}.json`, {
     cache: "no-store",
@@ -42,7 +39,6 @@ export const getChampionDetail = async (
 };
 
 export const getRotation = async (): Promise<ChampionRotation> => {
-  console.log("로테이션 번호 API 호출 시간:", new Date().toISOString());
   const res: Response = await fetch(MY_BUILD_URL, {
     method: "GET",
   });
@@ -51,7 +47,6 @@ export const getRotation = async (): Promise<ChampionRotation> => {
 };
 
 export const getItem = async (): Promise<fetchReturnValue<Item>> => {
-  console.log("아이템 API 호출 시간:", new Date().toISOString());
   const res: Response = await fetch(GET_ITEMS_URL, {
     cache: "force-cache",
   });
