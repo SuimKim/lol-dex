6c77215031496290d255824ef9b2071e77ac4576
SuimKim <rrrr6563@naver.com>
2025-03-19 02:54:57 +0900

    feat: 리스트 전환 기능 추가

--- src/app/(page)/rotations/client.page.tsx
+++ src/app/(page)/rotations/client.page.tsx
@@ -8,6 +8,9 @@ import ListGridContainer from "@/components/common/ListGridContainer";
 import Title from "@/components/common/Title";
 import Spacer from "@/components/common/Spacer";
 import { CHAMPION, CHAMPION_LIST_IMG_PATH } from "@/constants";
+import { useEffect, useState } from "react";
+import { Champion } from "@/types/shared/riot.api.types";
+import Text from "@/components/common/Text";
 
 const RotationClientPage = () => {
   const {
@@ -19,32 +22,48 @@ const RotationClientPage = () => {
     rotationListForNewUser,
   } = useRotation();
 
+  const [viewList, setViewList] = useState<Champion[]>([]);
+  const [isForNewUserList, setIsForNewUserList] = useState<boolean>(false); // 최초는 rotationList
+
+  useEffect(() => {
+    if (rotationList && viewList.length === 0) {
+      setViewList(rotationList); // 빈 리스트일 때만 설정
+    }
+  }, [rotationList, viewList]);
+
+  const handleChangeList = () => {
+    if (isForNewUserList) {
+      setViewList(rotationList || []);
+      setIsForNewUserList(false);
+    } else {
+      setViewList(rotationListForNewUser || []);
+      setIsForNewUserList(true);
+    }
+  };
+
   if (isError) throw error;
   if (isPending) return <Loading />;
 
   return (
     <>
-      <Spacer size="md" />
+      <Spacer size="lg" />
       <Title tag="h1" size="xl" margin="none" align="center">
-        금주의 무료 챔피언
+        {!isForNewUserList
+          ? "금주의 무료 챔피언"
+          : `신규유저 전용(레벨 ${maxNewUserLevel}까지)`}
       </Title>
+      <div
+        className="hover:text-blue-500 cursor-pointer pt-3"
+        onClick={handleChangeList}
+      >
+        <Text size="lg" align="center" oneLine>
+          {isForNewUserList
+            ? "👉🏻 금주의 무료 챔피언"
+            : ` 👉🏻 신규유저 전용(레벨 ${maxNewUserLevel}까지)`}
+        </Text>
+      </div>
       <ListGridContainer>
-        {rotationList!.map((item) => (
-          <Link href={`${CHAMPION}/${item.id}`} key={item.id}>
-            <ListCard
-              item={item}
-              imgPath={`${CHAMPION_LIST_IMG_PATH}/${item.id}_0.jpg`}
-              imgHeight={560}
-              imgWidth={308}
-            />
-          </Link>
-        ))}
-      </ListGridContainer>
-      <Title tag="h1" size="xl" margin="lg" align="center">
-        금주의 신규유저 무료 챔피언(레벨 {maxNewUserLevel}까지)
-      </Title>
-      <ListGridContainer>
-        {rotationListForNewUser!.map((item) => (
+        {viewList.map((item) => (
           <Link href={`${CHAMPION}/${item.id}`} key={item.id}>
             <ListCard
               item={item}
